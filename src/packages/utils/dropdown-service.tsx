7457c0083da7c572676deff9741f9108546063b7
llliuyang <1714610279@qq.com>
2021-02-01 20:53:38 +0800

    点击 dropdown option 的时候，自动关闭 dropdown service

--- src/packages/utils/dropdown-service.tsx
+++ src/packages/utils/dropdown-service.tsx
@@ -1,10 +1,19 @@
-import { computed, createApp, defineComponent, getCurrentInstance, onBeforeUnmount, onMounted, PropType, reactive, ref } from "vue";
+import { computed, createApp, defineComponent, getCurrentInstance, inject, onBeforeUnmount, onMounted, PropType, provide, reactive, ref } from "vue";
 import { defer } from "./defer";
 import './dropdown-service.scss';
+
 interface DropdownServiceOption {
   reference: MouseEvent | HTMLElement,
   content: () => JSX.Element
 }
+
+const DropdownServiceProvider = (() => {
+  const DROPDOWN_SERVICE_PROVIDER = '@@DROPDOWN_SERVICE_PROVIDER'
+  return {
+    provide: (handler: { onClick: () => void }) => provide(DROPDOWN_SERVICE_PROVIDER, handler),
+    inject: () => inject(DROPDOWN_SERVICE_PROVIDER) as { onClick: () => void }
+  }
+})()
 
 const ServiceComponent = defineComponent({
   props: { option: { type: Object as PropType<DropdownServiceOption>, required: true } },
@@ -80,6 +89,8 @@ const ServiceComponent = defineComponent({
 
     onBeforeUnmount(() => document.body.removeEventListener('mousedown', onMouseDownDocument, true))
 
+    DropdownServiceProvider.provide({ onClick: methods.hide })
+
     return () => (
       <div class={classes.value} style={styles.value} ref={el}>
         {state.option.content()}
@@ -94,12 +105,23 @@ export const DropdownOption = defineComponent({
   props: {
     label: { type: String },
     icon: { type: String },
-    customClass:{type: String}
+    customClass: { type: String }
   },
-  setup(props) {
+  emits: {
+    click: (e: MouseEvent) => true
+  },
+  setup(props, ctx) {
 
+    const { onClick: dropdownClickhandler } = DropdownServiceProvider.inject()
+
+    const handler = {
+      onClick: (e: MouseEvent) => {
+        ctx.emit('click', e)
+        dropdownClickhandler()
+      }
+    }
     return () => (
-      <div class={`dropdown-option dropdown-option-${props.customClass}`}>
+      <div class={`dropdown-option dropdown-option-${props.customClass}`} onClick={handler.onClick}>
         <i class={`iconfont ${props.icon}`} />
         <span>{props.label}</span>
       </div>
